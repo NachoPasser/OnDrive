1181259924b6aed599a908f316479521b2add746
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-06 11:25:48 -0300

    agrego - relaciones de la base de datos

--- api/src/db.js
+++ api/src/db.js
@@ -30,13 +30,54 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Trip, User } = sequelize.models;
+const { Car, Comment, Driver, Passenger, Roles, Trip, User } = sequelize.models;
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+//Relations
+//---------
+
+//User ---:M --> Rol
+//User <--:N--- Rol
+User.belongsToMany(Roles, {
+  through: "user_rol",
+  foreignKey: "user_id",
+  timestamps: false,
+});
+
+Roles.belongsToMany(User, {
+  through: "user_rol",
+  foreignKey: "rol_id",
+  timestamps: false,
+});
+
+//Driver ---1:M---> Rol
+Driver.belongsTo(Roles,{sourceKey:"id",foreignKey:"rol_id"});
+
+//Driver ---1:M ---> Car
+Driver.belongsTo(Car,{sourceKey:"id",foreignKey:"car_id"});
+
+//Passenger ---1:M---> Rol
+Passenger.belongsTo(Roles,{sourceKey:"id",foreignKey:"rol_id"});
+
+//Passenger / Driver / Trip --- 1:M ----> Comment
+Comment.belongsTo(Passenger,{sourceKey:"id",foreignKey:"passenger_id"});
+Comment.belongsTo(Driver,{sourceKey:"id",foreignKey:"driver_id"});
+Comment.belongsTo(Trip,{sourceKey:"id",foreignKey:"trip_id"});
+
+//Passenger M:N Trip
+Passenger.belongsToMany(Trip, {
+  through: "passenger_trip",
+  foreignKey: "passenger_id",
+  timestamps: false,
+});
+
+Trip.belongsToMany(Passenger, {
+  through: "passenger_trip",
+  foreignKey: "trip_id",
+  timestamps: false,
+});
+
 
 module.exports = {
-  Trip, User,
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
