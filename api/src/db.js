e3f6ff7d04261c7f8ae67680c916c5ef178c2f81
santiago alamos <santiagoalamos2000@gmail.com>
2022-07-07 14:51:38 -0300

    fix(api/src/db.js): Back listo para levantar

--- api/src/db.js
+++ api/src/db.js
@@ -6,37 +6,54 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
+//Para correr el Back, fue necesario colocar los Model:
+const ModelUser = require("./Models/User");
+const ModelCar = require("./Models/Car");
+const ModelTrip = require("./Models/Trip");
+const ModelComment = require("./Models/Comment");
+const ModelDriver = require("./Models/Driver");
+const ModelPassenger = require("./Models/Passenger");
+const ModelRoles = require("./Models/Role");
+
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ondrive`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
-const basename = path.basename(__filename);
 
-const modelDefiners = [];
+// SECCIÓN COMENTADA
+// const basename = path.basename(__filename);
+
+// const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/Models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/Models', file)));
-  });
+// fs.readdirSync(path.join(__dirname, '/Models'))
+//   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+//   .forEach((file) => {
+//     modelDefiners.push(require(path.join(__dirname, '/Models', file)));
+//   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
-sequelize.models = Object.fromEntries(capsEntries);
+// // Injectamos la conexion (sequelize) a todos los modelos
+// modelDefiners.forEach(model => model(sequelize));
+// // Capitalizamos los nombres de los modelos ie: product => Product
+// let entries = Object.entries(sequelize.models);
+// let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+// sequelize.models = Object.fromEntries(capsEntries);
+// FIN SECCIÓN COMENTADA
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-const { Trip, User } = sequelize.models;
+// También necesario para correr el Back:
+ModelUser(sequelize);
+ModelCar(sequelize);
+ModelTrip(sequelize);
+ModelComment(sequelize);
+ModelDriver(sequelize);
+ModelPassenger(sequelize);
+ModelRoles(sequelize);
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+const { user, car, trip, comment, driver, passenger, roles } = sequelize.models;
+// Relaciones:
+
 
 module.exports = {
-  Trip, User,
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
