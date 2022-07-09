920925eb9842f75adf2d41143fce10a52d2e2fa0
NachoPasser <nachopasser@gmail.com>
2022-07-08 21:04:10 -0300

    Store y Provider añadidos

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-//import store from './store/store';
+import store from './redux/store/store'
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
   ,
   document.getElementById('root')
 );
