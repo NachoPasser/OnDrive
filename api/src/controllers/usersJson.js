e9f6418042923c9da5b4b6a2772e6d96b84b2697
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-11 01:28:14 -0300

    fix - eliminar declaracion de funcion duplicada

--- api/src/controllers/usersJson.js
+++ api/src/controllers/usersJson.js
@@ -134,25 +134,6 @@ const getTripById = (req,res) => {
     }
 }
 
-const getTripById = (req,res) => {
-    const {id} = req.params;
-    try {
-        //valid id
-        if(typeof id !== "string") throw new Error(`Invalid ID(must be a string)`)
-        if(id.length !== 32) throw new Error(`Invalid ID(${id})`);
-        //search trip
-        const trip = usersStatic.filter((trip) => trip.id === id);
-        if(trip.length === 0) throw new Error(`Trip ID(${id}) not found`);
-        //response ok
-        res.status(200);
-        res.json({message:"Trip was found",value:{trip:trip[0]}})
-    } catch (e) {
-        //response error
-        res.status(400);
-        res.json({message:`something has wrong: ${e.message}`, value: null})
-    }
-}
-
 
 function generateUUID() {
     var d = new Date().getTime();
