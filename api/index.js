deb283ae1fd02c96ee5341187e76b66f0f5ff6fa
NachoPasser <nachopasser@gmail.com>
2022-07-13 00:35:02 -0300

    Añado lo necesario para deploy en Heroku y Elephan

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const PORT = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('% listening at 3001'); // eslint-disable-line no-console
   });
 });
