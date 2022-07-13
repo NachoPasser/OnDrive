9793df93f76025598421349e8ab9689a236874a1
NachoPasser <nachopasser@gmail.com>
2022-07-13 01:40:48 -0300

    Cambios necesarios en el front para deploy

--- client/src/redux/actions/getTrips.js
+++ client/src/redux/actions/getTrips.js
@@ -3,7 +3,7 @@ export const GET_TRIPS = "GET_TRIPS";
 
 export const getTrips = (destination) => {
     return async function (dispatch) {
-        axios.get("http://localhost:3001/api/static"
+        axios.get("https://on-drive.herokuapp.com/api/static"
         ).then(trips => dispatch({ type: GET_TRIPS, payload: trips.data })
         ).catch(c => console.log(c))
     }
