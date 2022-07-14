f3805d4c2f672cdace008f25117c60bf64510961
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-14 17:48:56 -0300

    feat - agrego Admin a sequelize.models

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 //const { Modelo } = sequelize.models;
-const { User, Roles, Passenger, Trip, Driver, Comment, Car } = sequelize.models;
+const { User, Roles, Passenger, Trip, Driver, Comment, Car, Admin} = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
