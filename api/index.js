4d88891fdb7462e48b19f8cd857dff2c71be0c3f
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-14 17:49:19 -0300

    feat - inicializacion de administradores

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const { initAdmin } = require("./src/Models/utils/Admin");
 
 const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(PORT, async () => {
+    //init admin
+    await initAdmin();
     console.log(`% listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
