cf4082b66448e1b4e0e19e45bfb998d525d593cf
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-16 22:23:25 -0300

    feat - agrego la nueva funcion de publicar viajes a el contoller postTrip

--- api/src/controllers/tripController.js
+++ api/src/controllers/tripController.js
@@ -1,32 +1,14 @@
 const { Trip } = require("../Models/Trip");
-
-const tripHistory = async (req, res) => {
-  let existTrip = await Trip.findAll()
-    .then((data) => {
-      return data.map((trip) => {
-        return {
-          id: trip.trip_id,
-          start_date: trip.start_date,
-          finish_date: trip.finish_date,
-          origin: trip.origin,
-          destination: trip.destination,
-          price: trip.price,
-        };
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  let response = [...existTrip];
-  response = response.filter((trip) => trip.trip_id == req.params.id);
-
-  if (response.length > 0) res.json(response);
-  else res.status(404).json({ message: "Trip not found" });
-};
+const { createTripAsDriver } = require("../Models/utils/Creations");
 
 const postTrip = async (req, res) => {
-  await Trip.create(req.body);
-  res.send("create trip");
+  try {
+    const { user_id, trip } = req.body;
+    const publishedTrip = await createTripAsDriver(user_id, trip);
+    res.status(201).json(publishedTrip);
+  } catch (e) {
+    res.status(400).json({ error: `Server error: ${e.message}` });
+  }
 };
 
 const getTrips = async (req, res) => {
@@ -51,6 +33,30 @@ const getTrips = async (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+const tripHistory = async (req, res) => {
+  let existTrip = await Trip.findAll()
+    .then((data) => {
+      return data.map((trip) => {
+        return {
+          id: trip.trip_id,
+          start_date: trip.start_date,
+          finish_date: trip.finish_date,
+          origin: trip.origin,
+          destination: trip.destination,
+          price: trip.price,
+        };
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  let response = [...existTrip];
+  response = response.filter((trip) => trip.trip_id == req.params.id);
+
+  if (response.length > 0) res.json(response);
+  else res.status(404).json({ message: "Trip not found" });
 };
 
 module.exports = {
