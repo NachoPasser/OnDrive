e70a0700e808989fb2899ae11ecc0d4e662386a6
MatiasPereson <mjpereson@gmail.com>
2022-07-18 10:32:59 -0300

    cambios para la demo

--- client/src/components/NavBar/navbarDrivers.jsx
+++ client/src/components/NavBar/navbarDrivers.jsx
@@ -13,21 +13,24 @@ export default function NavBar() {
     return (
         <ul className={style.nav}>
             <li className={style.liLogo}>
-                <NavLink className={style.navLink} exact to="/home" onClick={(e) => handleClick(e)}>
+                <NavLink className={style.navLink} exact to="/home-drivers" onClick={(e) => handleClick(e)}>
                     <img className={style.logo} src={logo} alt='No se encontró la imagen.' />
                 </NavLink>
             </li>
             <div className={style.buttons}>
-                <NavLink className={style.login} exact to="/login">Login</NavLink>
-                <NavLink className={style.register} exact to="/register">Register</NavLink>
+                {/* <NavLink className={style.login} exact to="/login">Login</NavLink>
+                <NavLink className={style.register} exact to="/register">Register</NavLink> */}
                 <button className={style.logout} onClick={() => window.localStorage.clear()}>Logout</button>
             </div>
             <div className={style.items}>
                 <li className={style.li}>
-                    <NavLink className={style.navLink} exact to="/home-passengers">Passengers</NavLink>
+                    <NavLink className={style.navLink} exact to="/recovery-password">Recuperar contraseña</NavLink>
                 </li>
                 <li className={style.li}>
-                    <NavLink className={style.navLink} exact to="/home-drivers">Drivers</NavLink>
+                    <NavLink className={style.navLink} exact to="/new-password">Cambiar contraseña</NavLink>
+                </li>
+                <li className={style.li}>
+                    <NavLink className={style.navLink} exact to="/home">Volver al home general</NavLink>
                 </li>
                 {/* <li className={style.li}>
                     <NavLink className={style.navLink} exact to="/community">Item 2</NavLink>
