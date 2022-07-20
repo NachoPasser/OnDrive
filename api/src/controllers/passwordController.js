5b848fac4dbbc105981fc8b86cf0b037b171594b
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-20 02:34:11 -0300

    feat - nuevo controller verifyRecoveryCode

--- api/src/controllers/passwordController.js
+++ api/src/controllers/passwordController.js
@@ -1,20 +1,41 @@
 const nodemailer = require("nodemailer");
 const { User } = require("../Models/User");
+const { findUserById, findUserByEmail } = require("../Models/utils/Finders");
+const {
+  generateRecoveryCode,
+  compareRecoveryCode,
+  resetCode,
+} = require("../Models/utils/Password");
 const bcrypt = require("bcrypt");
-const { isCorrectCredentials } = require("../Models/utils/Confirmer");
 const { SECRET_KEY } = process.env;
 const jwt = require("jsonwebtoken");
+
+let timeout = null;
 
 const recoverPassword = async (req, res) => {
   const { email } = req.body;
   try {
-    const user = await User.findOne({ where: { email } });
+    const user = await findUserByEmail({ email, model: true });
 
     if (!user) {
       return res
-        .status(409)
-        .json({ error: "No existe un usuario registrado con ese mail." });
+        .status(404)
+        .json({ error: "No existe un usuario registrado con ese Mail" });
     }
+
+    //si tengo un codigo no permito generar otro
+    if (user["recovery"] !== null)
+      return res
+        .status(404)
+        .json({ error: "Espere 5 minutos para solicitar otro codigo" });
+
+    const code = generateRecoveryCode();
+    const hashCode = await bcrypt.hash(code, 10);
+    user.set({ recovery: hashCode });
+    await user.save();
+
+    timeout = resetCode({ user, mins: 2 }); //permitir nuevo codigo en 5 mins
+
     const transporter = nodemailer.createTransport({
       service: "gmail",
       port: 456,
@@ -25,50 +46,76 @@ const recoverPassword = async (req, res) => {
       },
     });
     const mailOptions = {
-      from: '"Recuperación de contraseña" <ondrive.staff@gmail.com>',
+      from: '"OnDrive" <ondrive.staff@gmail.com>',
       to: email,
       subject: "Recuperación de contraseña",
-      text: `Hola ${user.name} ${user.last_name}, tu contraseña es: ${user.password}`,
+      text: `Hola ${user.name} ${user.last_name}.`,
+      html: `<p>
+      Codigo de recuperacion: <strong>${code}</strong>
+      <br>
+      <p>Este codigo se autodestruira en <strong>5 minutos</strong> 💣</p>
+      <br>
+      <br>
+      🙋 Saludos, el equipo de OnDrive. 
+      </p>`,
     };
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
+    transporter.sendMail(mailOptions, function (error) {
+      error ? console.error(error) : console.log(`📫 Mail enviado a ${email}`);
     });
-    res.send("Email enviado");
+    res.json({ message: "Mail enviado, revisa tu correo" });
   } catch (e) {
-    res.status(400).json({ error: `${e.message}` });
+    res.status(500).json({ error: e.message });
   }
 };
 
 const changePass = async (req, res) => {
-  const { token } = req.params;
-  let id = 0;
-  jwt.verify(token, SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.json({ message: "Token inválida." });
-    } else {
-      id = decoded.id;
+  try {
+    const { token } = req.params;
+    let id = 0;
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "Token inválida." });
+      } else {
+        id = decoded.id;
+      }
+    });
+    const { password } = req.body;
+    const user = await findUserById({ user_id: id, model: true });
+    if (!user) {
+      return res.status(404).send("Usuario no encontrado");
     }
-  });
-  const { password } = req.body;
-  const user = await User.findOne({
-    where: {
-      user_id: id,
-    },
-  });
-  if (!user) {
-    return res.status(400).send("Usuario no encontrado");
+    const passwordHash = await bcrypt.hash(password, 10);
+    user.password = passwordHash;
+    await user.save();
+    res.json({ message: "Contraseña cambiada" });
+  } catch (e) {
+    res.status(500).json({ error: "Error al cambiar la contraseña" });
   }
-  const passwordHash = await bcrypt.hash(password, 10);
-  user.password = passwordHash;
-  await user.save();
-  res.send("Contraseña cambiada");
+};
+
+const verifyRecoveryCode = async (req, res) => {
+  try {
+    const { code, email } = req.body;
+    if (!code || !email)
+      return res.status(400).json({ error: "Faltan datos(Email o Codigo)" });
+    // validar codigo con el de la db
+    const [valid, id] = await compareRecoveryCode({ email, code, timeout });
+    if (!valid)
+      return res.status(400).json({ error: "Codigo invalido o caducado" });
+
+    //devolver token de acceso si es valido
+    const token = jwt.sign({ id }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token }); //return token JWT
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 };
 
 module.exports = {
   recoverPassword,
   changePass,
+  verifyRecoveryCode,
 };
