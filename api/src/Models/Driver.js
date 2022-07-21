02b499d04602fd13518d6ae9d93e2e9f5b80a05d
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-21 12:47:06 -0300

    feat - nuevo campo virtual en Driver: Se agregar rating como nuevo campo virtual,  y se agregaron 'votes' y 'points' en driver.

--- api/src/Models/Driver.js
+++ api/src/Models/Driver.js
@@ -17,10 +17,29 @@ const Driver = conn.define(
       type: DataTypes.STRING(45),
       allowNull: false,
     },
-    rating: {
+    votes: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 0,
+    },
+    points: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    rating: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return this.points / this.votes;
+      },
+      set(newPoints) {
+        if (typeof newPoints !== "number")
+          throw new Error("El rating debe ser un numero");
+        if (newPoints < 0 || newPoints > 5)
+          throw new Error("El rating debe ser un numero entre 0 y 5");
+        this.votes += 1;
+        this.points += newPoints;
+      },
     },
   },
   {
