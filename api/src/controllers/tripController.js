3b175ca437d18edd539848884441c742202bbbe3
NachoPasser <nachopasser@gmail.com>
2022-07-23 06:51:46 -0300

    Perfil y Reseñas hechos

--- api/src/controllers/tripController.js
+++ api/src/controllers/tripController.js
@@ -1,6 +1,5 @@
-const { Trip } = require("../Models/Trip");
-const { createTripAsDriver } = require("../Models/utils/Creations");
-const { findAllTrips, findTripById } = require("../Models/utils/Finders");
+const { createTripAsDriver, createReview, updateReview } = require("../Models/utils/Creations");
+const { findAllTrips, findTripById, findReview, findAllReviews} = require("../Models/utils/Finders");
 
 const postTrip = async (req, res) => {
   try {
@@ -32,33 +31,101 @@ const getTripById = async (req, res) => {
   }
 };
 
-const tripHistory = async (req, res) => {
-  let existTrip = await Trip.findAll()
-    .then((data) => {
-      return data.map((trip) => {
-        return {
-          id: trip.trip_id,
-          start_date: trip.start_date,
-          finish_date: trip.finish_date,
-          origin: trip.origin,
-          destination: trip.destination,
-          price: trip.price,
-        };
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  let response = [...existTrip];
-  response = response.filter((trip) => trip.trip_id == req.params.id);
+//---------------------TODO LO DE ABAJO ES PARA RESEÑAS------------------------
+const reviewTrip = async (req, res) => {
+  try{
+    const {user_id, trip_id, review} = req.body
+    const trip = await createReview({user_id, trip_id, review})
+    res.json({trip_reviewed: trip})
+  } catch (e) {
+    res.status(400).json({ error: `${e.message}` });
+  }
+}
 
-  if (response.length > 0) res.json(response);
-  else res.status(404).json({ message: "Trip not found" });
-};
+
+const getTripReview = async (req, res) => { //no lo estoy usando pero lo dejo acá por si acaso
+  try{
+    let user_id = req.headers['user_id']
+    let trip_id = req.headers['trip_id']
+    const new_review = await findReview(user_id, trip_id)
+    res.json(new_review)
+  } catch (e) {
+    res.status(400).json({ error: `${e.message}` });
+  }
+}
+
+const getAllTripsReviews = async(req, res) => {
+  try{
+    let user_id = req.headers['user_id']
+    const allReviews = await findAllReviews(user_id)
+    res.json(allReviews)
+  } catch (e) {
+    res.status(400).json({ error: `${e.message}` });
+  }
+}
+
+const updateReviewTrip = async (req, res) => {
+  try{
+    const {user_id, trip_id, review} = req.body
+    const new_review = await updateReview({trip_id, user_id, review})
+    res.json({new_review})
+  } catch (e) {
+    res.status(400).json({ error: `${e.message}` });
+  }
+}
+
+const updateGeneralTripReview = async (req, res) => {
+  try{
+    const {trip_id} = req.body
+    const trip = await findTripById({trip_id, model:true})
+    let reviews = JSON.parse(JSON.stringify(trip.getDataValue('reviews'), null, 2)) //obtengo reviews y lo paso a obj, con getDataValue porque trip es un objeto
+    let sumOfRatings = reviews[0].rating //inicio la suma de Ratings con la primer review porque puede ser la unica
+    if(reviews.length > 1){
+      const initialValue = 0;
+      sumOfRatings = reviews.reduce((previousValue, currentValue) => previousValue + currentValue.rating,
+        initialValue
+      );
+    }
+    trip.update({rating: sumOfRatings/reviews.length}) //actualizo el rating del viaje
+    trip.save()
+    res.json(trip)
+  } catch(e){
+    res.status(400).json({ error: `${e.message}` });
+  }
+}
+
+// const tripHistory = async (req, res) => {
+//   let existTrip = await Trip.findAll()
+//     .then((data) => {
+//       return data.map((trip) => {
+//         return {
+//           id: trip.trip_id,
+//           start_date: trip.start_date,
+//           finish_date: trip.finish_date,
+//           origin: trip.origin,
+//           destination: trip.destination,
+//           price: trip.price,
+//         };
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+//   let response = [...existTrip];
+//   response = response.filter((trip) => trip.trip_id == req.params.id);
+
+//   if (response.length > 0) res.json(response);
+//   else res.status(404).json({ message: "Trip not found" });
+// };
 
 module.exports = {
-  tripHistory,
+  // tripHistory,
   postTrip,
   getTrips,
   getTripById,
+  reviewTrip,
+  updateReviewTrip,
+  updateGeneralTripReview,
+  getTripReview,
+  getAllTripsReviews
 };
---
+++ client/src/components/Sections/Review/Review.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect  }from 'react'
+import Rating from '@mui/material/Rating';
+import axios from 'axios'
+import { API_URL } from '../../../config/enviroment';
+import './Review.css'
+
+function Review({user_id, trip_id, actualRating, actualComment}) { //actualRating y actualComment me traen el rating y comentario actual de la reseña
+    const [rating, setRating] = useState(0)
+    const [comment, setComment] = useState('')
+    useEffect(() => {
+      //ACLARACION: Pregunto si son undefined ya que si el usuario no reseño el viaje entonces actualRating y actualComment son undefined
+      if(actualRating !== undefined) setRating(actualRating)
+      if(actualComment !== undefined) setComment(actualComment) 
+      //Si actualRating no es undefined entonces muestro el rating que el usuario puso anteriormente
+      //Si actualComment no es undefined entonces muestro el rating que el usuario puso anteriormente
+    }, [])
+
+    const handleSubmit = async () => {
+      try {
+        if(actualRating !== undefined){ //si la review existe entonces la actualizo, sino, la creo
+          await axios.put(`${API_URL}/trip/review`, {user_id, trip_id, review: {rating: rating, comment: comment}})
+        } else{
+          await axios.post(`${API_URL}/trip/review`, {user_id, trip_id, review: {rating: rating, comment: comment}})
+        }
+        await axios.put(`${API_URL}/trip/generalReview`, {trip_id}) 
+        //generalReview: actualiza el rating del viaje considerando todas las reviews sumadas a ese viaje hasta el momento
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+  return ( //En este div tengo Rating (componete de Mui) que muestra las estrellitas, el input que es donde pongo el comentario y un boton para submitear la reseña
+    <div id='mainDiv' style={{position: 'relative', bottom: '8px'}}>
+      <Rating
+      value={rating}
+      onChange={(e, newRating) => setRating(newRating)}
+      />
+      <input type="text" value={comment} onChange={(e) => setComment(e.target.value)}/>
+      <button onClick={handleSubmit}>Publicar reseña</button>
+    </div>
+  )
+}
+
+export default Review
