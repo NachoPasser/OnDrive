18db4624dbc55ac575cb4bdb63c60f912a328559
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-25 02:46:31 -0300

    feat - funciones para restringir permisos para publicar o comprar viajes en el panel admin

--- client/src/components/Sections/User/User.jsx
+++ client/src/components/Sections/User/User.jsx
@@ -1,34 +1,54 @@
-import React, {useState} from 'react'
-import axios from 'axios';
-import s from './User.module.css';
-import {API_URL} from '../../../config/enviroment';
-export default function User({email, ban_status}) {
-  const [banned, setBan] = useState(ban_status)
+import React, { useState } from "react";
+import axios from "axios";
+import s from "./User.module.css";
+import { API_URL } from "../../../config/enviroment";
 
-  const handleBan = () => {
-    setBan(true)
-    axios.put(`${API_URL}/admin/ban`, {ban_email: email})
-    .then(res => alert(res.data.msg))
-  }
+export default function User({
+  email,
+  ban_status,
+  ban_publish = false,
+  ban_purchase = false,
+}) {
+  const [banned, setBan] = useState(ban_status);
+  const [bannedPublish, setBanPublish] = useState(ban_publish);
+  const [bannedPurchase, setBanPurchase] = useState(ban_purchase);
 
-  const handleUnban = () => {
-    setBan(false)
-    axios.put(`${API_URL}/admin/unban`, {unban_email: email})
-    .then(res => alert(res.data.msg))
-  }
+  const types = {
+    ban_status: () => [!banned, setBan],
+    ban_publish: () => [!bannedPublish, setBanPublish],
+    ban_purchase: () => [!bannedPurchase, setBanPurchase],
+  };
+
+  const handleBanStatus = (e) => {
+    const { name } = e.target;
+    let [status, setter] = types[name]();
+
+    setter(status);
+
+    axios
+      .put(`${API_URL}/admin/${status ? "ban" : "unban"}`, {
+        [`${status ? "ban" : "unban"}_email`]: email,
+        type: name,
+      })
+      .then((res) => alert(res.data.msg));
+  };
 
   return (
-    <div style={{position: 'relative'}}>
-    <div className={s.columns}>
-      <span id={s.email}>{email}</span>
-      {!banned ? <span id={s.not_ban}>Activo</span> : <span id={s.ban}>Baneado</span>}
+    <div style={{ position: "relative" }}>
+      <div className={s.columns}>
+        <span id={s.email}>{email}</span>
+      </div>
+      <div id={s.divBtn}>
+        <button onClick={handleBanStatus} name="ban_status">
+          {banned ? "Desbanear" : "Banear"}
+        </button>
+        <button onClick={handleBanStatus} name="ban_publish">
+          {bannedPublish ? "Desbanear" : "Banear"}
+        </button>
+        <button onClick={handleBanStatus} name="ban_purchase">
+          {bannedPurchase ? "Desbanear" : "Banear"}
+        </button>
+      </div>
     </div>
-    <div id={s.divBtn}>
-      {!banned 
-      ? <button className={s.btn} onClick={handleBan}>BANEAR</button> 
-      : <button className={s.btn} onClick={handleUnban}>DESBANEAR</button>}
-    </div>
-    </div>
-  )
+  );
 }
-
