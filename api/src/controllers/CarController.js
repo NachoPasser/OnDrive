aef29d0dfb3715fc15e034d26cedcb5d26658190
Marco Poggi <marcopoggi2017@gmail.com>
2022-07-27 21:36:50 -0300

    feat - se obtiene el archivo temporal

--- api/src/controllers/CarController.js
+++ api/src/controllers/CarController.js
@@ -1,31 +1,44 @@
-const {Driver} = require('../Models/Driver');
-const {Car} = require('../Models/Car')
-const {createCar} = require('../Models/utils/Creations'); 
-const {findUserById} = require('../Models/utils/Finders');
-const { SECRET_KEY } = process.env;
-const jwt = require("jsonwebtoken");
+const { Driver } = require("../Models/Driver");
+const { Car } = require("../Models/Car");
+const { createCar } = require("../Models/utils/Creations");
+const { findUserById } = require("../Models/utils/Finders");
 
-const addCar = async(req, res) => {
-    try {
-        const { user_id, decoded, car } = req.body;
-        const {driver} = user_id 
-        ? await findUserById({user_id, model: false, driver: true})
-        : await findUserById({user_id: decoded.id, model: false, driver: true})
-        const carCreated = await createCar(driver.driver_id, car);
-        res.json(carCreated);
-    } catch (e) {
-        res.status(400).json({message: e.message});
-    }
-}
+const addCar = async (req, res) => {
+  try {
+    const { user_id, decoded } = req.body;
+    const { model, brand, type, license_plate, year, color, fuel } = req.body;
 
-const getCars = async(req, res) => {
-    const cars = await Car.findAll({
-        include: Driver
-    });
-    res.json(cars);
-}
+    let car = {
+      file: req.file.path,
+      model,
+      brand,
+      type,
+      license_plate,
+      year,
+      color,
+      fuel,
+    };
+
+    const { driver } = user_id
+      ? await findUserById({ user_id, model: false, driver: true })
+      : await findUserById({ user_id: decoded.id, model: false, driver: true });
+   
+    const carCreated = await createCar(driver.driver_id, car);
+    res.json(carCreated);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ message: e.message });
+  }
+};
+
+const getCars = async (req, res) => {
+  const cars = await Car.findAll({
+    include: Driver,
+  });
+  res.json(cars);
+};
 
 module.exports = {
-    addCar,
-    getCars
-}
+  addCar,
+  getCars,
+};
